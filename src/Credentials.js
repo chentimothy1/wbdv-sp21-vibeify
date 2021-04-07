6b1053f3218157a0d670e32541e891caca47c06f
chentimothy1 <timothychen1@protonmail.com>
2021-04-07 13:04:09 -0700

    all files

---
+++ src/Credentials.js
@@ -0,0 +1,11 @@
+const Credentials = () => {
+
+    //change to whatever your credentials are
+    return {
+        ClientId: '1b2680cc967d45ca999452fbcad4f25d',
+        ClientSecret: '925d23c600ac4a10bff1c0604d0b43a0'
+    }
+}
+
+export { Credentials };
+
